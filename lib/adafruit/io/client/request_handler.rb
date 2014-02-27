10d3410b9be8c3e08cccc3b7860858ff1993bb67
Justin Cooper <jwcooper@gmail.com>
2014-02-26 22:56:23 -0600

    handle put and modify post

--- lib/adafruit/io/client/request_handler.rb
+++ lib/adafruit/io/client/request_handler.rb
@@ -2,21 +2,29 @@ module Adafruit
   module IO
     class Client
       module RequestHandler
-        def handle_get(url, options = {})
-          response = conn.get do |req|                           # GET http://sushi.com/search?page=2&limit=100
+        def handle_get(url, data = nil, options = {})
+          response = conn.get do |req|
             req.url "api/#{url}"
             #req.params['limit'] = 100
           end
           return response.body
         end
 
-        def handle_post(url, options = {})
-          response = conn.post do |req|                           # GET http://sushi.com/search?page=2&limit=100
-            req.url '/api/feeds'
-            req.body = options
+        def handle_post(url, data, options = {})
+          response = conn.post do |req|
+            req.url "api/#{url}"
+            req.body = data
           end
           return response.body
         end
+
+        def handle_put(url, data, options = {})
+          response = conn.put do |req|
+            req.url "api/#{url}"
+            req.body = data
+          end
+          return response.body
+        end        
       end
     end
   end
