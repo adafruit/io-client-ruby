43450edf0ab1610c5000b481bc379f1cf1e0f784
Justin Cooper <jwcooper@gmail.com>
2015-01-23 13:14:00 -0600

    url encode strings

--- lib/adafruit/io/client/request_handler.rb
+++ lib/adafruit/io/client/request_handler.rb
@@ -1,10 +1,12 @@
+require 'open-uri'
+
 module Adafruit
   module IO
     class Client
       module RequestHandler
         def handle_get(url, data = nil, options = {})
           response = conn.get do |req|
-            req.url "api/#{url}"
+            req.url URI::encode("api/#{url}")
             #req.params['limit'] = 100
           end
           return response.body
@@ -12,7 +14,7 @@ module Adafruit
 
         def handle_post(url, data, options = {})
           response = conn.post do |req|
-            req.url "api/#{url}"
+            req.url URI::encode("api/#{url}")
             req.body = data
           end
           return response.body
@@ -20,7 +22,7 @@ module Adafruit
 
         def handle_put(url, data, options = {})
           response = conn.put do |req|
-            req.url "api/#{url}"
+            req.url URI::encode("api/#{url}")
             req.body = data
           end
           return response.body
@@ -28,7 +30,7 @@ module Adafruit
 
         def handle_delete(url, options = {})
           response = conn.delete do |req|
-            req.url "api/#{url}"
+            req.url URI::encode("api/#{url}")
           end
           return response.status
         end 
