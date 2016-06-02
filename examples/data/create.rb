b0b8e6ae961267c04e80a4f12ab4d2cabc864813
Adam Bachman <adam.bachman@gmail.com>
2016-06-02 10:15:45 -0400

    update README and data/create example to clarify IO::Client API
    fixes #4

--- examples/data/create.rb
+++ examples/data/create.rb
@@ -1,7 +1,13 @@
 require 'adafruit/io'
 
 # create an instance
-aio = Adafruit::IO::Client.new :key => 'AIO_KEY_HERE'
+aio = Adafruit::IO::Client.new :key => ENV['AIO_KEY']
 
+# use `create` to target an existing Feed
 data = aio.feeds("Temperature").data.create({:value => 11})
+
+# or use `send_data` to send a single value and auto-create the Feed if necessary
+data2 = aio.feeds("Temperature").data.send_data(12)
+
 puts data.inspect
+puts data2.inspect
