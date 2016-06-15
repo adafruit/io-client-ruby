51985278dcdc753e060e0fa5fe11939847002626
Adam Bachman <adam.bachman@gmail.com>
2016-06-15 10:39:16 -0400

    update examples/data/read so that it doesn't blow up if feed doesn't have data

--- examples/data/read.rb
+++ examples/data/read.rb
@@ -6,10 +6,14 @@ aio = Adafruit::IO::Client.new :key => ENV['ADAFRUIT_IO_KEY']
 feed = aio.feeds.retrieve.first
 data = aio.feeds(feed.key).data.retrieve.first
 
-puts %[\
-FEED  %s
-FIRST DATA
-  ID         %s
-  CREATED AT %s
-  VALUE      %s
-] % [feed.name, data.value, data.created_at, data.feed_id]
+if data
+  puts %[\
+  FEED  %s
+  FIRST DATA
+    ID         %s
+    CREATED AT %s
+    VALUE      %s
+  ] % [feed.name, data.value, data.created_at, data.feed_id]
+else
+  puts "NO DATA IN #{ feed.name }"
+end
