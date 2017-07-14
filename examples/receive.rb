dfded71bb23c860d044193624c66e00d66b7778f
Adam Bachman <adam.bachman@gmail.com>
2017-07-14 17:43:38 -0400

    drop unused examples

--- examples/receive.rb
+++
@@ -1,4 +0,0 @@
-require 'adafruit/io'
-
-aio = Adafruit::IO::Client.new :key => 'API_KEY_HERE'
-puts aio.receive("Temperature")
--- examples/send.rb
+++
@@ -1,4 +0,0 @@
-require 'adafruit/io'
-
-aio = Adafruit::IO::Client.new :key => 'API_KEY_HERE'
-aio.send_data("Temperature", 22)
