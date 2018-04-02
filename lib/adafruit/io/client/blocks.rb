4398c0e62aeca7600876927993b56250237af7d7
Adam Bachman <adam.bachman@gmail.com>
2018-04-02 13:57:47 -0400

    complete block client API tests, fix some bugs in block client API

--- lib/adafruit/io/client/blocks.rb
+++ lib/adafruit/io/client/blocks.rb
@@ -15,7 +15,7 @@ module Adafruit
         def block(*args)
           username, arguments = extract_username(args)
           dashboard_key = require_argument(arguments, :dashboard_key)
-          block_id = get_id_from_arguments(arguments)
+          block_id = get_key_from_arguments(arguments)
 
           get api_url(username, 'dashboards', dashboard_key, 'blocks', block_id)
         end
@@ -25,13 +25,13 @@ module Adafruit
           dashboard_key = require_argument(arguments, :dashboard_key)
           block_attrs = arguments.shift
 
-          post api_url(username, 'groups'), group_attrs
+          post api_url(username, 'dashboards', dashboard_key, 'blocks'), block_attrs
         end
 
         def delete_block(*args)
           username, arguments = extract_username(args)
           dashboard_key = require_argument(arguments, :dashboard_key)
-          block_id = get_id_from_arguments(arguments)
+          block_id = get_key_from_arguments(arguments)
 
           delete api_url(username, 'dashboards', dashboard_key, 'blocks', block_id)
         end
@@ -39,11 +39,33 @@ module Adafruit
         def update_block(*args)
           username, arguments = extract_username(args)
           dashboard_key = require_argument(arguments, :dashboard_key)
-          block_id = get_id_from_arguments(arguments)
+          block_id = get_key_from_arguments(arguments)
 
           query = get_query_from_arguments(arguments, %w(name properties visual_type column row size_x size_y block_feeds))
 
           put api_url(username, 'dashboards', dashboard_key, 'blocks', block_id), query
+        end
+
+        # The list of every valid property name that can be stored by Adafruit IO.
+        #
+        # Not every property applies to every block. Boolean values should be
+        # stored as the string "yes" or "no".
+        def valid_block_properties
+          %w(
+            text value release fontSize onText offText backgroundColor onColor
+            offColor min max step label orientation handle minValue maxValue
+            ringWidth label xAxisLabel yAxisLabel yAxisMin yAxisMax tile
+            fontColor showName showTimestamp errors historyHours showNumbers
+            showLocation
+          )
+        end
+
+        # The list of every valid block type that can be presented by Adafruit IO.
+        def valid_block_visual_types
+          %w(
+            toggle_button slider momentary_button gauge line_chart text
+            color_picker map stream image remote_control
+          )
         end
 
       end
