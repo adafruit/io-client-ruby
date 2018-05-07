94f68c2a07c526d94c290726a544edf5c136a3f9
Adam Bachman <adam.bachman@gmail.com>
2018-05-07 15:14:59 -0400

    fill out group specs

--- spec/adafruit/io/group_spec.rb
+++ spec/adafruit/io/group_spec.rb
@@ -32,9 +32,81 @@ RSpec.describe Adafruit::IO::Client do
     expect(groups).not_to be_empty
   end
 
-  it 'creates a group'
-  it 'adds a feed to a group'
-  it 'updates a group'
-  it 'deletes a group'
+  it 'creates a group' do
+    mock_response(
+      path: api_path('groups'),
+      method: :post,
+      status: 200,
+      with_request_body: true,
+      body: mock_group_json,
+    )
+
+    group = @aio.create_group(key: 'group-key', name: 'Group Name', description: "group description")
+    expect(group).not_to be_empty
+  end
+
+  it 'updates a group' do
+    mock_response(
+      path: api_path('groups', 'group-key'),
+      method: :put,
+      status: 200,
+      body: mock_group_json,
+    )
+
+    group = @aio.update_group(key: 'group-key', name: 'Group Other Name')
+    expect(group).not_to be_empty
+  end
+
+  it 'deletes a group' do
+    mock_response(
+      path: api_path('groups', 'group-key'),
+      method: :delete,
+      status: 200,
+      body: mock_group_json
+    )
+
+    result = @aio.delete_group(key: 'group-key')
+    expect(result).to_not be_empty
+  end
+
+  it 'adds a feed to a group' do
+    mock_response(
+      path: api_path('groups', 'group-key', 'add'),
+      method: :post,
+      status: 200,
+      with_request_body: true,
+      body: mock_group_json,
+    )
+
+    result = @aio.group_add_feed('group-key', 'other.feed-key')
+    expect(result).to_not be_empty
+  end
+
+  it 'removes a feed from a group' do
+    mock_response(
+      path: api_path('groups', 'group-key', 'remove'),
+      method: :post,
+      status: 200,
+      with_request_body: true,
+      body: mock_group_json,
+    )
+
+    result = @aio.group_remove_feed('group-key', 'feed-key')
+    expect(result).to_not be_empty
+  end
+
+  it 'raises on failure to remove a feed from a group' do
+    mock_response(
+      path: api_path('groups', 'non-available-key', 'remove'),
+      method: :post,
+      status: 404,
+      with_request_body: true,
+      body: '{"error":"not found - API documentation can be found at https://io.adafruit.com/api/docs"}',
+    )
+
+    expect {
+      @aio.group_remove_feed('non-available-key', 'feed-key')
+    }.to raise_error(Adafruit::IO::RequestError)
+  end
 
 end
